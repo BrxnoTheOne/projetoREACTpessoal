1010750beded8299e623af9588856fc1dcdb7114
BrxnoTheOne <operacamper@gmail.com>
2025-04-03 17:18:45 -0300

    aula 03/04 react.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import fessor from './fotojordan.jpg'
+import Card from './Card'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="exemplo">
+      <header>
+        <h1>title</h1>
+        <nav>
+
+       <a>exemplo1</a>
+       <a>exemplo2</a>
+
+        </nav>
       </header>
-    </div>
-  );
+      
+        <Card />
+     
+        <Card />
+      
+        <Card />
+      </div>
+
+      );
 }
 
-export default App;
+      export default App;
