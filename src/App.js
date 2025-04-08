3a7c94028a9545d05e71a25bb53c66d2c9b3d86c
bruno rocha <operacamper@gmail.com>
2025-04-08 09:53:55 -0300

    edição no appjs e adição de banner com defeito

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 
 import Header from './components/Header';
+import Banner from './components/Header/Banner';
 import Footer from './components/Header/Footer';
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
 
     <>
     <Header />
+    <Banner />
     <h1>Hello world !</h1>
     <p>O melhor site para assistir seus filmes preferidos !</p>
     <Footer />
